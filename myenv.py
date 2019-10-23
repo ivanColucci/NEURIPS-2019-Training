6d9317f015792278eb02f5ac4a18fa201a39db53
peppepel95 <peppe95.pellegrino@gmail.com>
2019-10-23 20:53:23 +0200

    creazione parallela + default reward

--- myenv.py
+++ myenv.py
@@ -38,3 +38,62 @@ class RewardShapingEnv(L2M2019Env):
 
     def distance_and_energy(self):
         return 100*self.distance_reward() - self.energy_consumption_reward()/22
+
+    def standard_reward(self):
+        state_desc = self.get_state_desc()
+        if not self.get_prev_state_desc():
+            return 0
+
+        reward = 0
+        dt = self.osim_model.stepsize
+
+        # alive reward
+        # should be large enough to search for 'success' solutions (alive to the end) first
+        reward += self.d_reward['alive']
+
+        # effort ~ muscle fatigue ~ (muscle activation)^2
+        ACT2 = 0
+        for muscle in sorted(state_desc['muscles'].keys()):
+            ACT2 += np.square(state_desc['muscles'][muscle]['activation'])
+        self.d_reward['effort'] += ACT2*dt
+        self.d_reward['footstep']['effort'] += ACT2*dt
+
+        self.d_reward['footstep']['del_t'] += dt
+
+        # reward from velocity (penalize from deviating from v_tgt)
+
+        p_body = [state_desc['body_pos']['pelvis'][0], -state_desc['body_pos']['pelvis'][2]]
+        v_body = [state_desc['body_vel']['pelvis'][0], -state_desc['body_vel']['pelvis'][2]]
+        v_tgt = self.vtgt.get_vtgt(p_body).T
+
+        self.d_reward['footstep']['del_v'] += (v_body - v_tgt)*dt
+
+        # footstep reward (when made a new step)
+        if self.footstep['new']:
+            # footstep reward: so that solution does not avoid making footsteps
+            # scaled by del_t, so that solution does not get higher rewards by making unnecessary (small) steps
+            reward_footstep_0 = self.d_reward['weight']['footstep']*self.d_reward['footstep']['del_t']
+
+            # deviation from target velocity
+            # the average velocity a step (instead of instantaneous velocity) is used
+            # as velocity fluctuates within a step in normal human walking
+            #reward_footstep_v = -self.reward_w['v_tgt']*(self.footstep['del_vx']**2)
+            reward_footstep_v = -self.d_reward['weight']['v_tgt']*np.linalg.norm(self.d_reward['footstep']['del_v'])/self.LENGTH0
+
+            # panalize effort
+            reward_footstep_e = -self.d_reward['weight']['effort']*self.d_reward['footstep']['effort']
+
+            self.d_reward['footstep']['del_t'] = 0
+            self.d_reward['footstep']['del_v'] = 0
+            self.d_reward['footstep']['effort'] = 0
+
+            reward += reward_footstep_0 + reward_footstep_v + reward_footstep_e
+
+        # success bonus
+        if not self.is_done() and (self.osim_model.istep >= self.spec.timestep_limit): #and self.failure_mode is 'success':
+            # retrieve reward (i.e. do not penalize for the simulation terminating in a middle of a step)
+            #reward_footstep_0 = self.d_reward['weight']['footstep']*self.d_reward['footstep']['del_t']
+            #reward += reward_footstep_0 + 100
+            reward += 10
+
+        return reward
