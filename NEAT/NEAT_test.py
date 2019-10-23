9bcfec89b5e1e4672c6b3f18cf2cfdd4a2bfb00e
ivanColucci <i.colucci2@studenti.unisa.it>
2019-10-23 10:36:44 +0200

    update NEAT_test

--- NEAT/NEAT_test.py
+++ NEAT/NEAT_test.py
@@ -3,17 +3,18 @@ import random
 import neat
 import pickle
 from NEAT.utils.utilities import eval_genome
+from NEAT.my_reproduction import TournamentReproduction
 random.seed(1234)
 
 
 def test(source='winner_genome', load_from_checkpoint=False, checkpoint='neat-checkpoint'):
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'Configs/config-osim')
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+    config = neat.Config(neat.DefaultGenome, TournamentReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
 
     if load_from_checkpoint:
         p = neat.Checkpointer.restore_checkpoint(checkpoint)
-        pe = neat.ParallelEvaluator(1, eval_genome)
+        pe = neat.ParallelEvaluator(None, eval_genome)
         winner = p.run(pe.evaluate, 1)
         with open(source, 'wb') as f:
             pickle.dump(winner, f)
@@ -24,7 +25,6 @@ def test(source='winner_genome', load_from_checkpoint=False, checkpoint='neat-ch
     result = eval_genome(winner_net, config, visual=True, is_a_net=True)
     print(result)
 
-
 if __name__ == '__main__':
-    test(source='../winner_genome_16_10', load_from_checkpoint=False)
+    test(source='../results/winner_genome_distance_reward', load_from_checkpoint=False)
     # test(source='winner_genome_distance', load_from_checkpoint=True, checkpoint='neat-checkpoint-232')
