8942013c62902ee849beaf2271c1874c4171dfa8
ivanColucci <i.colucci2@studenti.unisa.it>
2019-10-24 10:41:40 +0200

    Update my_env

--- myenv.py
+++ myenv.py
@@ -38,3 +38,33 @@ class RewardShapingEnv(L2M2019Env):
 
     def distance_and_energy(self):
         return 100*self.distance_reward() - self.energy_consumption_reward()/22
+
+    def get_observation(self):
+        obs_dict = self.get_observation_dict()
+        res = []
+
+        res.append(obs_dict['pelvis']['height'])
+        res.append(obs_dict['pelvis']['pitch'])
+        res.append(obs_dict['pelvis']['roll'])
+        res.append(obs_dict['pelvis']['vel'][0] / self.LENGTH0)
+        res.append(obs_dict['pelvis']['vel'][1] / self.LENGTH0)
+        res.append(obs_dict['pelvis']['vel'][2] / self.LENGTH0)
+        res.append(obs_dict['pelvis']['vel'][3])
+        res.append(obs_dict['pelvis']['vel'][4])
+        res.append(obs_dict['pelvis']['vel'][5])
+
+        for leg in ['r_leg', 'l_leg']:
+            res += obs_dict[leg]['ground_reaction_forces']
+            res.append(obs_dict[leg]['joint']['hip_abd'])
+            res.append(obs_dict[leg]['joint']['hip'])
+            res.append(obs_dict[leg]['joint']['knee'])
+            res.append(obs_dict[leg]['joint']['ankle'])
+            res.append(obs_dict[leg]['d_joint']['hip_abd'])
+            res.append(obs_dict[leg]['d_joint']['hip'])
+            res.append(obs_dict[leg]['d_joint']['knee'])
+            res.append(obs_dict[leg]['d_joint']['ankle'])
+            for MUS in ['HAB', 'HAD', 'HFL', 'GLU', 'HAM', 'RF', 'VAS', 'BFSH', 'GAS', 'SOL', 'TA']:
+                res.append(obs_dict[leg][MUS]['f'])
+                res.append(obs_dict[leg][MUS]['l'])
+                res.append(obs_dict[leg][MUS]['v'])
+        return res
