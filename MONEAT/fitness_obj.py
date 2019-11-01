cd7af74e0b24c3d7a8a686099506e98f4ca1434b
peppepel95 <peppe95.pellegrino@gmail.com>
2019-11-01 12:13:26 +0100

    FIRST: fitness obj

--- MONEAT/fitness_obj.py
+++ MONEAT/fitness_obj.py
@@ -37,25 +37,31 @@ def sum_vector(vector):
 MAX_ENERGY = 10000
 MAX_DISTANCE = 5
 
+
 class FitnessObj():
 
     def __init__(self, distance, energy):
         self.distance = distance
-        self.energy = MAX_DISTANCE*(MAX_ENERGY-energy)/MAX_ENERGY
+        self.energy_dissipated = energy
+        self.energy_remaining = self.take_remaining(energy)
 
-    #**************COMPARATIVE OPERATORS*******************
+    @staticmethod
+    def take_remaining(energy_dissipated):
+        return MAX_DISTANCE * (MAX_ENERGY - energy_dissipated) / MAX_ENERGY
+
+    # ************** COMPARATIVE OPERATORS *******************
     def __lt__(self, other):
         if type(other) is float or type(other) is int:
             return self.distance < other
-        my_pareto_dist = math.sqrt(math.pow(self.distance, 2) + math.pow(self.energy, 2))
-        other_pareto_dist = math.sqrt(math.pow(other.distance, 2) + math.pow(other.energy, 2))
+        my_pareto_dist = math.sqrt(math.pow(self.distance, 2) + math.pow(self.energy_remaining, 2))
+        other_pareto_dist = math.sqrt(math.pow(other.distance, 2) + math.pow(other.energy_remaining, 2))
         return my_pareto_dist < other_pareto_dist
 
     def __eq__(self, other):
         if type(other) is float or type(other) is int:
             return self.distance == other
-        my_pareto_dist = math.sqrt(math.pow(self.distance, 2) + math.pow(self.energy, 2))
-        other_pareto_dist = math.sqrt(math.pow(other.distance, 2) + math.pow(other.energy, 2))
+        my_pareto_dist = math.sqrt(math.pow(self.distance, 2) + math.pow(self.energy_remaining, 2))
+        other_pareto_dist = math.sqrt(math.pow(other.distance, 2) + math.pow(other.energy_remaining, 2))
         return my_pareto_dist == other_pareto_dist
 
     def __ge__(self, other):
@@ -67,36 +73,44 @@ class FitnessObj():
     def __gt__(self, other):
         return not self <= other
 
-    #***************MATHEMATICAL OPERATORS**********************
+    # *************** MATHEMATICAL OPERATORS **********************
     def __add__(self, other):
         if type(other) is float or type(other) is int:
-            return FitnessObj(distance=(self.distance.__add__(other)), energy=MAX_ENERGY-((MAX_ENERGY-self.energy).__add__(other)))
-        return FitnessObj(distance=(self.distance+other.distance), energy=MAX_ENERGY-((MAX_ENERGY-self.energy)+(MAX_ENERGY-other.energy)))
+            return FitnessObj(distance=(self.distance + other),
+                              energy=self.energy_dissipated)
+        return FitnessObj(distance=(self.distance + other.distance),
+                          energy=(self.energy_dissipated + other.energy_dissipated))
 
     def __sub__(self, other):
         if type(other) is float or type(other) is int:
-            return FitnessObj(distance=(self.distance.__sub__(other)), energy=MAX_ENERGY-((MAX_ENERGY-self.energy).__sub__(other)))
-        return FitnessObj(distance=(self.distance.__sub__(other.distance)), energy=(MAX_ENERGY-self.energy).__sub__(MAX_ENERGY-other.energy))
+            return FitnessObj(distance=(self.distance - other),
+                              energy=self.energy_dissipated)
+        return FitnessObj(distance=(self.distance - other.distance),
+                          energy=(self.energy_dissipated - other.energy_dissipated))
 
     def __truediv__(self, other):
         if type(other) is float or type(other) is int:
-            return FitnessObj(distance=(self.distance.__truediv__(other)), energy=MAX_ENERGY-((MAX_ENERGY-self.energy).__truediv__(other)))
-        return FitnessObj(distance=(self.distance.__truediv__(other.distance)), energy=MAX_ENERGY-((MAX_ENERGY-self.energy).__truediv__(MAX_ENERGY-other.energy)))
+            return FitnessObj(distance=(self.distance.__truediv__(other)),
+                              energy=self.energy_dissipated)
+        return FitnessObj(distance=(self.distance.__truediv__(other.distance)),
+                          energy=self.energy_dissipated.__truediv__(other.energy_dissipated))
 
     def __mul__(self, other):
         if type(other) is float or type(other) is int:
-            return FitnessObj(distance=(self.distance.__mul__(other)), energy=MAX_ENERGY-((MAX_ENERGY-self.energy).__mul__(other)))
-        return FitnessObj(distance=(self.distance*other.distance), energy=MAX_ENERGY-((MAX_ENERGY-self.energy).__mul__(MAX_ENERGY-other.energy)))
+            return FitnessObj(distance=(self.distance.__mul__(other)),
+                              energy=self.energy_dissipated)
+        return FitnessObj(distance=(self.distance.__mul__(other.distance)),
+                          energy=self.energy_dissipated.__mul__(other.energy_dissipated))
 
-    #**************OTHER OPERATORS********************
+    # ************** OTHER OPERATORS ********************
     def __str__(self):
-        return 'D: {} E: {}'.format(str(round(self.distance, 2)), str(round(MAX_ENERGY-self.energy, 2)))
+        return 'D: {} E: {}'.format(str(round(self.distance, 2)), str(round(self.energy_dissipated, 2)))
 
     def __copy__(self):
-        return FitnessObj(self.distance, self.energy)
+        return FitnessObj(self.distance, self.energy_dissipated)
 
     def __format__(self, format_spec):
         return self.__str__()
 
     def __round__(self, n=None):
-        return FitnessObj(round(self.distance, n), round(self.energy, n))
+        return FitnessObj(round(self.distance, n), round(self.energy_dissipated, n))
