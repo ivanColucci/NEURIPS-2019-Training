ec087dced76d99e3753bc35fdcb22aebbc0b9328
ivanColucci <i.colucci2@studenti.unisa.it>
2019-12-10 11:31:41 +0100

    Update NSTrain

--- NSTrain.py
+++ NSTrain.py
@@ -67,4 +67,4 @@ def start(out_file, restore_checkpoint=False, checkpoint='NS-checkpoint-', trial
 
 
 if __name__ == '__main__':
-    start('winner_genome', restore_checkpoint=False, trials=1, elite=True)
+    start('winner_genome', restore_checkpoint=False, trials=1, elite=False)
