0726449f55bac6188b4626fe14c536a9821e1860
peppepel95 <32591694+peppepel95@users.noreply.github.com>
2020-01-15 09:31:43 +0100

    train

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ random.seed(1234)
 np.random.seed(1234)
 
 # constants
-n_max_gen = None
+n_max_gen = 500
 n_workers = None
 
 
@@ -31,7 +31,7 @@ def reverse_activation(z):
     return -z
 
 
-def run(config_file, out_file='winner_genome', restore_checkpoint=False, checkpoint='neat-checkpoint'):
+def run(config_file, out_file='winner_genome', restore_checkpoint=False, checkpoint='neat-checkpoint', winner='0'):
     config = neat.Config(MyGenome, EliteReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_file)
@@ -45,30 +45,36 @@ def run(config_file, out_file='winner_genome', restore_checkpoint=False, checkpo
         p = ElitePopulation(config, overwrite=True)
         p.allow_regeneration(False)
     # Add a stdout reporter to show progress in the terminal.
-    p.add_reporter(FileReporter(True, "output.txt"))
+    p.add_reporter(FileReporter(True, "output" + winner + ".txt"))
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
-    p.add_reporter(MyCheckpointer(checkpoint_interval=50, overwrite=True))
+    p.add_reporter(MyCheckpointer(checkpoint_interval=50, filename_prefix='neat-checkpoint-' + winner, overwrite=True))
     #   1 - distance metric
     #   2 - area metric
     #   3 - step reward with a bonus for staying with the pelvis between 0.84 and 0.94
     #   4 - step reward
     #   5 - Definitive
     #   6 - body in range incremental
-    evaluator = Evaluator(reward_type=5, visual=False, old_input=False, steps=5000)
-    pe = ParallelEvaluator(n_workers, evaluator.eval_genome, timeout=500)
+    evaluator = Evaluator(reward_type=5, visual=False, old_input=False, steps=1000)
+    pe = ParallelEvaluator(n_workers, evaluator.eval_genome, timeout=240)
     winner = p.run(pe.evaluate, n_max_gen)
     # Save the winner
     with open(out_file, 'wb') as f:
         pickle.dump(winner, f)
 
 
-def start(out_file, restore_checkpoint=False, checkpoint='neat-checkpoint'):
+def start(out_file, restore_checkpoint=False, checkpoint='neat-checkpoint', trials=1):
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config_human0')
-    run(config_path, restore_checkpoint=restore_checkpoint, out_file=out_file, checkpoint=checkpoint)
-
+    for i in range(trials):
+        seed = 1234 + i
+        random.seed(seed)
+        np.random.seed(seed)
+        if restore_checkpoint:
+            run(config_path, out_file=out_file, checkpoint=checkpoint, winner=str(i))
+        else:
+            run(config_path, out_file=out_file, winner=str(i))
 
 if __name__ == '__main__':
-    start('winner_genome', restore_checkpoint=False, checkpoint='neat-checkpoint-')
+    start('winner_genome', restore_checkpoint=False, checkpoint='neat-checkpoint-', trials=5)
